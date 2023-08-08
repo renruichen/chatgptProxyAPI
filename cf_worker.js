80790cfe21c71fa1800d02972e0852ad524be5a7
renruichen <60461363+renruichen@users.noreply.github.com>
2023-08-09 02:23:42 +0800

    Update cf_worker.js

--- cf_worker.js
+++ cf_worker.js
@@ -1,4 +1,4 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
+const TELEGRAPH_URL = 'https://api.openai.com/proxy';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
